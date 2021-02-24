652bbcaddab7802fead9a881f327e4228256a601
Rob Rua <robertrua@gmail.com>
2021-02-23 22:18:47 -0500

    add template without main

---
+++ python-library/{{cookiecutter.name}}/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+
+install_requires = []
+
+version_file = Path(__file__).parent.joinpath(
+    "{{cookiecutter.module_name}}", "VERSION.txt"
+)
+version = version_file.read_text(encoding="UTF-8").strip()
+
+with open("README.md", "r", encoding="UTF-8") as in_file:
+    long_description = in_file.read()
+
+setup(
+    name="{{cookiecutter.module_name}}",
+    version=version,
+    author="{{cookiecutter.author_name}}",
+    author_email="{{cookiecutter.author_email}}",
+    description="{{cookiecutter.description}}",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=install_requires,
+    include_package_data=True,
+)
